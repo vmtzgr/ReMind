b2623a7ad160437f2192fd20cf3ea1856c5b0eef
vmtzgr <vladii@me.com>
2022-04-28 15:17:36 +0200

    Post methode hinzugefügt, design änderungen(minimal)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 const log = console.log;
 const container = document.querySelector(".entries");
 
+const delPost = (id) => {
+  fetch("http://localhost:3000/posts/" + id, {
+    method: "DELETE",
+    /*      body: JSON.stringify(),
+      headers: {
+        "Content-Type": "application/json",
+      }, */
+  });
+  console.log(id);
+};
+
 const renderPosts = async () => {
   let uri = "http://localhost:3000/posts?_sort=id&_order=desc";
 
@@ -10,10 +21,15 @@ const renderPosts = async () => {
 
   let temp = "";
   posts.forEach((post) => {
-    temp += ` <div class="blog-entrie">
-                  <h2>${post.title}</h2>
-                  <p>${post.body}</p>
-              </div>`;
+    temp += ` 
+      <div class="blog-entrie">
+        <div class="card-header">
+          <h2>${post.title}</h2>
+          <button class="del-btn" type="submit"  onclick="delPost(${post.id})"><img src="./asstes/img/del-btn.svg" alt="Delete Posts"  ></button>
+        </div>
+        <p>${post.body}</p>
+      </div>
+`;
   });
   container.innerHTML += temp;
 };
